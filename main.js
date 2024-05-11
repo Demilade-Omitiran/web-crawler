528dc7a825da658d15d525cab973f58af9a02ca1
Demilade Omitiran <omitirand@gmail.com>
2024-05-11 23:44:52 +0100

    Call crawlPage method after confirming that number of params is valid

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+import { crawlPage } from "./crawl.js";
+
+async function main() {
+  const numberOfParams = process.argv.length - 2;
+  let url;
+
+  if (numberOfParams != 1) {
+    console.log("Invalid number of params");
+    return;
+  } else {
+    url = process.argv[2];
+    console.log(`crawler starting at ${url}`);
+  }
+
+  await crawlPage(url);
+}
+
+main();
